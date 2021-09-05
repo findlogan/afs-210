e411d1aed4c658691d0caf8e1e3d0ff9b17ac4f9
findlogan <logaster2@hotmail.com>
2021-09-04 22:20:19 -0400

    tuple

--- week1/tuples/tuple.py
+++ week1/tuples/tuple.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
-class_list = ['Class-V', 'Class-VI', 'Class-VII', 'Class-VIII']
-id_list = [1, 2, 2, 3]
+class_list = ('Class-V', 'Class-VI', 'Class-VII', 'Class-VIII')
+id_list = (1, 2, 2, 3)
 temp = defaultdict(set)
 for c, i in zip(class_list, id_list):
     temp[c].add(i)
