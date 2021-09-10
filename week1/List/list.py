00dcf4b05376490915f52aa3f70b853b356fb510
findlogan <logaster2@hotmail.com>
2021-09-09 21:19:34 -0400

    Fixed List output

--- week1/List/list.py
+++ week1/List/list.py
@@ -1,3 +1,12 @@
 list = [25, 45, 36, 47, 69, 48, 68, 78, 14, 36]
+
+# Printing original for expected output part 1. 
+print("Original List:")
+print(list)
+
+# Sort the list to expected output...
 list.sort()
+
+# Print modified list for expected output part 2.
+print("Sorted List:")
 print(list)
