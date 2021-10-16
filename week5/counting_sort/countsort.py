6855c24af8d7f2279188ebcb2d88df2c98b868b8
findlogan <logaster2@hotmail.com>
2021-10-15 23:13:42 -0400

    Countsort

--- week5/counting_sort/countsort.py
+++ week5/counting_sort/countsort.py
@@ -1,14 +1,24 @@
+# In the counting sort, you provide a list and the highest value that is in that list
 def counting_sort(alist, max_val):
+
+    # m is going to be the maximum value + 1
     m = max_val + 1
     count = [0] * m                
     
     for x in alist:
-    # count occurences of x in alist
+    # count occurences of x number in alist
         count[x] += 1             
     i = 0
+
+    # For each number in the range of max val + 1
     for x in range(m):            
+        
+        # For each occurance of X in the range
         for c in range(count[x]):  
+            
+            # Set the current index equal to the number we're currently looking at
             alist[i] = x
+            # Increment i for the index for next number
             i += 1
     
     # When done, return the sorted list back with all the numbers
